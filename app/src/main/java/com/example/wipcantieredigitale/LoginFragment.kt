e5a7baa0c4eac67b07a2f941ad76d02972592f98
Paolo <paolo.benitezpaccha@studenti.unicampania.it>
2019-04-20 15:00:56 +0200

    Piccole modifiche a LoginFragment. NOTA: aggiungere il metodo per nascondere la tastiera

--- app/src/main/java/com/example/wipcantieredigitale/LoginFragment.kt
+++ app/src/main/java/com/example/wipcantieredigitale/LoginFragment.kt
@@ -1,20 +1,11 @@
 package com.example.wipcantieredigitale
 
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.app.Activity
-import android.support.v4.content.ContextCompat.getSystemService
-import android.support.v4.content.ContextCompat.getSystemService
-import android.os.IBinder
-import android.content.Context.INPUT_METHOD_SERVICE
-import android.support.v4.content.ContextCompat.getSystemService
-import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
--- app/src/main/java/com/example/wipcantieredigitale/MainActivity.kt
+++ app/src/main/java/com/example/wipcantieredigitale/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.wipcantieredigitale
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 }
