6db97c0660a1ecc79cb322cbb98bd04a60718c14
Paolo <paolo.benitezpaccha@studenti.unicampania.it>
2019-05-05 04:04:14 +0200

    modifica grafica fragment_aggiungi_compito

--- app/src/main/java/com/example/wipcantieredigitale/SplashScreen.kt
+++ app/src/main/java/com/example/wipcantieredigitale/SplashScreen.kt
@@ -24,7 +24,7 @@ class SplashScreen : Fragment() {
 
         Handler().postDelayed({
             Navigation.findNavController(view).navigate(R.id.action_splashScreen_to_welcomeFragment)
-        }, 2000)
+        }, 1500)
 
     }
 
